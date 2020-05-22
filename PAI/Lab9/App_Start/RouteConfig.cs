86119b51785bc060e03715a66ab447dafdd13e65
inf136768 <maciej.meler@student.put.poznan.pl>
2020-05-22 19:10:41 +0200

    add lab9

---
+++ PAI/Lab9/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Lab9
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default29",
+                url: "{controller}",
+                defaults: new {controller = "Songs", action = "Index"}
+            );
+            
+            routes.MapRoute(
+                name: "Route2",
+                url:"{controller}/{action}",
+                defaults: new { controller = "Songs", action = "Square", id = 23}
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
